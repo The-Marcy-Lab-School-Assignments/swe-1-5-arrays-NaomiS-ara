bd60ee2a2c50c306f99d6f9765594ee6ed282a9c
[NaomiS-ara] <[Naomispencer101@gmail.com]>
2025-10-15 20:41:26 -0400

    swe-1-5-arrays-Naomi

--- src/debug.js
+++ src/debug.js
@@ -2,10 +2,20 @@
 const clearArr = (arr) => {
   arr = [];
 };
+function clearArr(arr) {
+  arr.length = 0;
+}
+
+
 
 const getFirstItem = (array) => {
   return array.shift();
 };
+function getFirstItem(arr) {
+  return arr[0];
+}
+
+
 
 module.exports = {
   clearArr,
--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,26 +1,74 @@
 const addToFrontOrBack = () => {
 };
+function addToFrontOrBack(arr, value, isFront) {
+  if (isFront) {
+    arr.unshift(value);
+  } else {
+    arr.push(value);
+  }
+}
+
+//let myArray = [1, 2, 3];
+addToFrontOrBack(myArray, 0, true);
+console.log(myArray);  // Should log: [0, 1, 2, 3]
+
+addToFrontOrBack(myArray, 4, false);
+console.log(myArray);  // Should log: [0, 1, 2, 3, 4]
+
+
 
 const reverseString = () => {
 };
+function reverseString(str) {
+return str.split("").reverse().join("");
+
 
 const newArrayFullOf = () => {
-};
+}; 
+function newArrayFullOf(value, numOfValue) {
+  return new Array(numOfValue).fill(value);
+}
+
 
 const insertIntoMiddle = () => {
 };
+function insertIntoMiddle(arr, value) {
+  const middleIndex = Math.floor(arr.length / 2);
+  arr.splice(middleIndex, 0, value);
+}
+
+
 
 const deleteFromMiddle = () => {
 };
+function deleteFromMiddle(arr) {
+  const middleIndex = Math.floor(arr.length / 2);
+  arr.splice(middleIndex, 1);
+}
+
+
 
 const isRightIndex = () => {
 };
+function isRightIndex(arr, value, index) {
+  return arr[index] === value;
+}
+
+
 
 const roundAllNumsDown = () => {
 };
+function roundAllNumsDown(arr) {
+  return arr.map(num => Math.floor(num));
+}
+
 
 const getAllYCoordinates = () => {
 };
+function getAllYCoordinates(arrOfCoords) {
+  return arrOfCoords.map(coord => coord[1]);
+}
+
 
 module.exports = {
   addToFrontOrBack,
@@ -32,3 +80,4 @@ module.exports = {
   roundAllNumsDown,
   getAllYCoordinates,
 };
+}
